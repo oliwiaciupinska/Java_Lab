18c576a96ec0d1d7b52c9fff1c919daaa79953bb
Oliwia Ciupińska <oliwiaciupinska1@gmail.com>
2025-05-30 20:18:14 +0200

    Add files via upload

---
+++ Laboratorium_Java_1/src/Zadania_Lab_1/Zadanie1.java
@@ -0,0 +1,11 @@
+package Zadania_Lab_1;
+
+public class Zadanie1 {
+
+    String imie = "Oliwia";
+    int wiek = 21;
+
+    public void zad1(){
+        System.out.println("Moje imie to " + imie + " i mam " + wiek  + " lat");
+    }
+}
